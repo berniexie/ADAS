1e47ecb11fef3e594e5d2dda07507cd3c5f8de7d
Bernard Xie <bxie94@gmail.com>
2015-03-29 13:47:59 -0700

    pushing a working index.php

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # ADAS
 CSCI 310 Project Part 2 "Another Day Another Scholar"
+
+* Program will be using PHP, Slim, and Twig.
+** To install, you might have to run composer install/composer update
---
+++ index.php
@@ -0,0 +1,17 @@
+<?php
+require "vendor/autoload.php";
+
+Twig_Autoloader::register();
+$loader = new Twig_Loader_Filesystem('./views/templates');
+$twig = new Twig_Environment($loader, array(
+#    'cache' => './tmp/cache',  # turned off for development purposes
+));
+session_cache_limiter(false);
+session_start();
+$app = new \Slim\Slim();
+
+$app->get('/', function () use ($app, $twig) {	
+  echo "hello, world";
+});
+
+$app->run();
