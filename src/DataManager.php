70c24f61b241d4558f77028cf011637e1941350d
Jenny Yoshihara <jyoshiha@usc.edu>
2015-04-05 00:13:06 -0700

    Cast info to strings in API class

--- src/DataManager.php
+++ src/DataManager.php
@@ -8,127 +8,129 @@ class DataManager
 {
 	private $words = array();	    //array of Word objects that will be passed back to the cloud
 	private $papers = array();      //array of Paper objects used
-    private $cloud;
-    private $apiManager;
-    private $paperIdMap = array();  //maps paperID (string) to paper objects
+	private $cloud;
+	private $apiManager;
+	private $paperIdMap = array();  //maps paperID (string) to paper objects
 
-  public function __construct()
-  {
-    $this->apiManager = new ResearchPaperAPI();
-  }
+	public function __construct()
+	{
+		$this->apiManager = new ResearchPaperAPI();
+	}
 
-  //clears the array of paper objects for the next word cloud
-  public function clearPapers()
-  {
-    $this->papers = null;
-  }
+	public function cmp($a, $b){
+		if($a->getTotalFrequency() == $b->getTotalFrequency()){
+			return 0;
+		}
 
-  /*  TEMPORARILY UNIMPLEMENTED FOR SPRINT 1
-  public function getAutofillSuggestions($userInput)
-  {
-    // Passes autofill suggestions from apiManager
-    return $this->apiManager->getAuthorSuggestion($userInput);
-  }
-  */
+		return($a->getTotalFrequency() < $b->getTotalFrequency()) ? -1:1;
+	}
 
-  //This function is to count the frequency of the words in the papers and create the word objects
-  public function createWordObjects()
-  {
-    //searches through each paper
-    foreach($this->papers as $paper)
-    {
-      //gets the individual contents from the papers
-      $content = $paper->getContent();
+	//clears the array of paper objects for the next word cloud
+	public function clearPapers()
+	{
+		$this->papers = null;
+	}
 
-      //searches through each word in the paper
-      foreach($content as $text=>$frequency)
-      {
-        $bool = false;
+	/*  TEMPORARILY UNIMPLEMENTED FOR SPRINT 1
+	public function getAutofillSuggestions($userInput)
+	{
+		// Passes autofill suggestions from apiManager
+		return $this->apiManager->getAuthorSuggestion($userInput);
+	}
+	*/
 
-        foreach($this->words as $word)
-        {
-          //if there's a match, increment the frequency of the word in the paper
-          if($text == $word->getTerm())
-          {
-            $word->incrementFrequency($paper->getId(), $frequency); //the key word has been found in the paper
-            $bool = true; 
-          }
-        }
+	//This function is to count the frequency of the words in the papers and create the word objects
+	public function createWordObjects()
+	{
+		//searches through each paper
+		foreach($this->papers as $paper)
+		{
+			//gets the individual contents from the papers
+			$content = $paper->getContent();
 
-        //if no match is found, a word object is initialized for that word and added to the array
-        if($bool == false)
-        {
-          $word = new Word($text, $paper->getId(), $frequency);
-          $this->words[] = $word;
-        }
-      }
-    }
-  }
+			//searches through each word in the paper
+			foreach($content as $text => $frequency)
+			{
+				$flag = false;
 
-  //This function creates a cloud based on a key word
-  public function getCloudByKeyWord($keyWord, $limit){
-      $this->clearPapers();
+				foreach($this->words as $word)
+				{
+					//if there's a match, increment the frequency of the word in the paper
+					if($text == $word->getTerm())
+					{
+						$word->incrementFrequency($paper->getId(), $frequency); //the key word has been found in the paper
+						$flag = true; 
+					}
+				}
 
-      //sets array of papers using info from API
-      $this->papers = $this->apiManager->getPapersByKeyWords($keyWord, $limit);
+				//if no match is found, a word object is initialized for that word and added to the array
+				if($flag == false)
+				{
+					$word = new Word($text, $paper->getId(), $frequency);
+					$this->words[] = $word;
+				}
+			}
+		}
+	}
 
-      return $this->createWordCloud();
-  }
+	//This function creates a cloud based on a key word
+	public function getCloudByKeyWord($keyWord, $limit){
+		$this->clearPapers();
 
-  //This function creates a cloud based on an author
-  public function getCloudByAuthor($author, $limit)  {
-      clearPapers();
+		//sets array of papers using info from API
+		$this->papers = $this->apiManager->getPapersByKeyWords($keyWord, $limit);
 
-      //sets array of papers using API
-      $this->papers = $this->apiManager->getPapersByAuthor($author, $limit);
+		return $this->createWordCloud();
+	}
 
-      return $this->createWordCloud();
-  }
+	//This function creates a cloud based on an author
+	public function getCloudByAuthor($author, $limit)  {
+		clearPapers();
 
-/*  NOT FOR SPRINT 1
-  //This function creates a cloud based on a given subset of papers
-  public function getCloudByPapers(array $paperSubset){
-       clearPapers();
+		//sets array of papers using API
+		$this->papers = $this->apiManager->getPapersByAuthor($author, $limit);
 
-       return createWordCloud();
-  }
-*/
+		return $this->createWordCloud();
+	}
 
-  public function cmp($a, $b){
-      if($a->getTotalFrequency() == $b->getTotalFrequency()){
-          return 0;
-      }
+	/*  NOT FOR SPRINT 1
+	//This function creates a cloud based on a given subset of papers
+	public function getCloudByPapers(array $paperSubset){
+		clearPapers();
 
-      return($a->getTotalFrequency() < $b->getTotalFrequency()) ? -1:1;
-  }
+		return createWordCloud();
+	}
+	*/
 
-  //Generates the word cloud with the current array of word objects
-  public function createWordCloud(){
-      $this->createPaperMap();
-      $this->createWordObjects();
+	//Generates the word cloud with the current array of word objects
+	public function createWordCloud(){
+		$this->createPaperMap();
+		$this->createWordObjects();
 
-      //sort by frequency
-      uasort($words, "cmp");
-      //cuts off array at [magic number] elements
-      $cloudArray = $this->words;
-      $cloudArray = array_slice($cloudArray, 0, 250);
-      //make cloud
-      $this->cloud = new Cloud($cloudArray, $paperIdMap);
-      return $this->cloud;
-  }
+		//sort by frequency
+		uasort($this->words, array("DataManager", 'cmp'));
 
-  //Creates paper map
-  public function createPaperMap(){
-      foreach($this->$papers as $object){
-          $this->paperIdMap[$object->getId()] = $object; //key = ID, value = paper object
-      }
-  }
+		//cuts off array at [magic number] elements
+		$cloudArray = $this->words;
+		$cloudArray = array_slice($cloudArray, 0, 250);
 
-  // This passes the corresponding paper object to the front end
-  public function getPaperObject($paperId){
-      //use paper ID to access paper object
-      return $this->$paperIdMap[$paperId];
-  }
+		//make cloud
+		$this->cloud = new Cloud($cloudArray, $this->paperIdMap);
+		return $this->cloud;
+	}
+
+	//Creates paper map
+	public function createPaperMap(){
+		foreach($this->papers as $object){
+			$this->paperIdMap[$object->getId()] = $object; //key = ID, value = paper object
+		}
+	}
+
+	// This passes the corresponding paper object to the front end
+	public function getPaperObject($paperId){
+		//use paper ID to access paper object
+		return $this->$paperIdMap[$paperId];
+	}
 
 }
 
--- src/ResearchPaperAPI.php
+++ src/ResearchPaperAPI.php
@@ -73,7 +73,7 @@ class ResearchPaperAPI
             $content = array();
             foreach ($content_temp as $word) {
                 if (!in_array($word, $this->stop_words)) {
-                    $content[] = $word;
+                    $content[] = (string)$word; //cast to string
                 }
             }
             $content_temp = $content;
@@ -88,7 +88,7 @@ class ResearchPaperAPI
             }
             $link = $d->mdurl;
 
-            $p = new Paper($id, $authors, $title, $journal, $content, $link);
+            $p = new Paper((string)$id, $authors, (string)$title, (string)$journal, $content, (string)$link); //cast to string
             $papers[] = $p;
         }
         return $papers;
