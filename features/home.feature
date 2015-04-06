3c8a514c094eaf9f1463fa8dff472fc7141fef00
Jenny Yoshihara <jyoshiha@usc.edu>
2015-04-06 00:41:43 -0700

    Fixed Cucumber tests

---
+++ features/home.feature
@@ -0,0 +1,48 @@
+Feature: User visits the homepage
+	In order to verify that the application is running
+	As a user
+	I want to view the homepage
+
+#Checks that the necessary components are there
+Scenario: View homepage
+	When I enter the URL in a browser
+	Then I should see "Another Day Another Scholar"
+	And I should see a Keyword radio button
+	And I should see an Author radio button
+	And I should see a combo box
+	And I should see a Submit button
+	And I should see a search box
+
+Scenario: Check for default settings
+	Given I am on the homepage
+	Then I should see "keyword" as the radio button value
+	And I should see "10" as the combo box value
+
+Scenario: Check that radio button values can be changed
+	Given I am on the homepage
+	When I choose "author" from the radio buttons
+	Then I should see "author" as the radio button value
+
+Scenario: Check that combo box values can be changed
+	Given I am on the homepage
+	When I choose "20" as the combo box value
+	Then I should see "20" as the combo box value
+
+#Scenario: Check for error message when submitting empty search box
+#	Given I am on the homepage
+#	When I click the Submit button
+#	Then I should see "Please enter a valid keyword or author"
+
+Scenario: Search with keyword 'artificial intelligence'
+	Given I am on the homepage
+	When I choose "keyword" from the radio buttons
+	And I fill in "search" with "artificial intelligence"
+	And I click the Submit button
+	Then I should see "program"
+
+Scenario: Search with author 'Smith'
+	Given I am on the homepage
+	When I choose "author" from the radio buttons
+	And I fill in "search" with "Smith"
+	And I click the Submit button
+	Then I should see "application"
---
+++ features/step_definitions/steps.rb
@@ -0,0 +1,71 @@
+#Visits the URL
+When(/^I enter the URL in a browser$/) do
+	visit ui_url ''
+end
+
+#Sets condition that you are on the homepage
+Given(/^I am on the homepage$/) do
+	visit ui_url ''
+end
+
+#Checks if the page contains given content
+Then(/^I should see "(.*?)"$/) do |text|
+	page.should have_content text
+end
+
+#Checks that there is a Keyword radio button
+Then(/^I should see a Keyword radio button$/) do
+	page.should have_selector 'input[type=radio][name=type][value=keyword]'
+end
+
+#Checks that there is an Author radio button
+Then(/^I should see an Author radio button$/) do
+	page.should have_selector 'input[type=radio][name=type][value=author]'
+end
+
+#Checks that there is a combo box
+Then(/^I should see a combo box$/) do
+	page.should have_selector 'select[name=limit]'
+end
+
+#Checks that there is a submit button
+Then(/^I should see a Submit button$/) do
+	page.should have_selector 'input[type=submit][class=buttons][value=Submit]'
+end
+
+#Checks that there is a search box
+Then(/^I should see a search box$/) do
+	page.should have_selector 'input[type=text][id=tags][name=search]'
+end
+
+#Checks that the given radio button is checked
+Then(/^I should see "(.*?)" as the radio button value$/) do |arg1|
+#	page.should have_selector 'input[type=radio][name=type][value=' + arg1 + '][checked]'
+	find("input[type=radio][name=type][value=" + arg1 + "]").should be_checked
+end
+
+#Checks that the given value is the value of the combo box
+Then(/^I should see "(.*?)" as the combo box value$/) do |arg1|
+	expect(page).to have_select('limit', selected: arg1)
+end
+
+#Chooses the value of the radio button
+When(/^I choose "(.*?)" from the radio buttons$/) do |arg1|
+	find("input[type=radio][name=type][value=" + arg1 + "]").click
+end
+
+#Chooses the value of the combo box
+When(/^I choose "(.*?)" as the combo box value$/) do |arg1|
+	select(arg1, :from => 'limit')
+end
+
+#Clicks the Submit button
+When(/^I click the Submit button$/) do
+	find('input[type=submit][class=buttons][value=Submit]').click
+end
+
+#Fills in the search box with the given text
+When(/^I fill in "(.*?)" with "(.*?)"$/) do |arg1, arg2|
+	fill_in arg1, with: arg2
+end
+
