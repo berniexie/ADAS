db10b1b2e11f527616fd892df7bb4bd109f47812
Bernard Xie <bxie94@gmail.com>
2015-04-17 10:16:28 -0700

    javascript routing

--- main.js
+++ main.js
@@ -1,12 +1,16 @@
 function exportFunc() {
   var inputs = document.getElementsByTagName("input");
   var cbs = []; //will contain all checked checkboxes
+  var cbnums = "";
   for (var i = 0; i < inputs.length; i++) {
     if (inputs[i].type == "checkbox") {
       if(inputs[i].checked){
-        cbs.push(inputs[i]);
+        cbnums += i;
+        cbnums += ',';
       }
     }
   }
-  console.log("this happened" + cbs.length);
+  var keyWord = document.getElementById('paperHeader').getElementsByTagName('h2')[0].innerHTML;
+  keyWord = keyWord.substring(1, keyWord.length-1);
+  window.location.href = "http://localhost:3000/pdf/" + keyWord + "/" + cbnums;
 }
