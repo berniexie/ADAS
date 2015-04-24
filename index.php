1c8d355088bef2a662d271fc2e0a7f5bc85769ea
Matthew <matthew.obrien929@gmail.com>
2015-04-24 10:57:12 -0700

    turned off caching for dev

--- index.php
+++ index.php
@@ -6,7 +6,7 @@ require_once('./vendor/Twig/Autoloader.php');
 Twig_Autoloader::register();
 $loader = new Twig_Loader_Filesystem('./views/templates');
 $twig = new Twig_Environment($loader, array(
-   'cache' => './tmp/cache',  # turned off for development purposes
+   // 'cache' => './tmp/cache',  # turned off for development purposes
 ));
 session_cache_limiter(false);
 session_start();
