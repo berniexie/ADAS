f6b95caa4f767de4d38cb25ca9c49b7636b9d879
Bernard Xie <bxie94@gmail.com>
2015-04-29 10:44:18 -0700

    fixed js error

--- main.js
+++ main.js
@@ -1,61 +1,51 @@
 window.onload() = function() {
-  $(".printWordCloud").click(function() {
-    console.log("button clicked");
-    h2ml2canvas(document.getElementById("wordCloud"), {
-      onrendered: function(canvas) {
-        var image = new Image();
-        image.src = canvas.toDataURL("image/png");
-        window.location.href = image;
-    });
-  })
-}
-
-function exportFunc() {
-  var inputs = document.getElementsByTagName("input");
-  var cbnums = "";
-  var ttlnums = "";
-  for (var i = 0; i < inputs.length; i++) {
-    if (inputs[i].type == "checkbox") {
-      ttlnums += i;
-      ttlnums += ',';
-      if(inputs[i].checked){
-        cbnums += i;
-        cbnums += ',';
+  function exportFunc() {
+    var inputs = document.getElementsByTagName("input");
+    var cbnums = "";
+    var ttlnums = "";
+    for (var i = 0; i < inputs.length; i++) {
+      if (inputs[i].type == "checkbox") {
+        ttlnums += i;
+        ttlnums += ',';
+        if(inputs[i].checked){
+          cbnums += i;
+          cbnums += ',';
+        }
       }
     }
-  }
-  var keyWord = document.getElementById('paperHeader').getElementsByTagName('h2')[0].innerHTML;
-  keyWord = keyWord.substring(1, keyWord.length-1);
-  if(cbnums.length != 0){
-    //If boxes are checked, send indexes of the checked boxes
-    window.location.href = "http://localhost:3000/pdf/" + keyWord + "/" + cbnums;
-  } else {
-    //If none of the boxes are checked, send indexes of all boxes
-    window.location.href = "http://localhost:3000/pdf/" + keyWord + "/" + ttlnums;
-  }
-}
-
-function ssCloud() {
-  var inputs = document.getElementsByTagName("input");
-  var cbnums = "";
-  var ttlnums = "";
-  for (var i = 0; i < inputs.length; i++) {
-    if (inputs[i].type == "checkbox") {
-      ttlnums += i;
-      ttlnums += ',';
-      if(inputs[i].checked){
-        cbnums += i;
-        cbnums += ',';
-      }
+    var keyWord = document.getElementById('paperHeader').getElementsByTagName('h2')[0].innerHTML;
+    keyWord = keyWord.substring(1, keyWord.length-1);
+    if(cbnums.length != 0){
+      //If boxes are checked, send indexes of the checked boxes
+      window.location.href = "http://localhost:3000/pdf/" + keyWord + "/" + cbnums;
+    } else {
+      //If none of the boxes are checked, send indexes of all boxes
+      window.location.href = "http://localhost:3000/pdf/" + keyWord + "/" + ttlnums;
     }
   }
-  var keyWord = document.getElementById('paperHeader').getElementsByTagName('h2')[0].innerHTML;
-  keyWord = keyWord.substring(1, keyWord.length-1);
-  if(cbnums.length != 0){
-    //If boxes are checked, send indexes of the checked boxes
-    window.location.href = "http://localhost:3000/sscloud/" + keyWord + "/" + cbnums;
-  } else {
-    //If none of the boxes are checked, send indexes of all boxes
-    window.location.href = "http://localhost:3000/sscloud/" + keyWord + "/" + ttlnums;
+
+  function ssCloud() {
+    var inputs = document.getElementsByTagName("input");
+    var cbnums = "";
+    var ttlnums = "";
+    for (var i = 0; i < inputs.length; i++) {
+      if (inputs[i].type == "checkbox") {
+        ttlnums += i;
+        ttlnums += ',';
+        if(inputs[i].checked){
+          cbnums += i;
+          cbnums += ',';
+        }
+      }
+    }
+    var keyWord = document.getElementById('paperHeader').getElementsByTagName('h2')[0].innerHTML;
+    keyWord = keyWord.substring(1, keyWord.length-1);
+    if(cbnums.length != 0){
+      //If boxes are checked, send indexes of the checked boxes
+      window.location.href = "http://localhost:3000/sscloud/" + keyWord + "/" + cbnums;
+    } else {
+      //If none of the boxes are checked, send indexes of all boxes
+      window.location.href = "http://localhost:3000/sscloud/" + keyWord + "/" + ttlnums;
+    }
   }
 }
